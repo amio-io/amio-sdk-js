17af5f9a4cfeaec382e83a15d4d58a352d8cccb9
zatziky <zatziky@gmail.com>
2018-04-13 12:56:55 +0200

    test: send a message

--- test/e2e/connector.test.js
+++ test/e2e/connector.test.js
@@ -1,44 +1,49 @@
-const {AmioConnector} = require('../../index')
-const expect = require('chai').expect
 const config = require('../config')
+const {AmioApi} = require('../../index')
+const expect = require('chai').expect
 const omit = require('ramda/src/omit')
 const assoc = require('ramda/src/assoc')
 
-let amioConnector = null
+let amioApi = null
 
-describe('Connector', function () {
+describe('Amio API Connector', function () {
 
   before(() => {
     const settings = {
       baseUrl: config.CONNECTOR_API_BASE_URL
     }
-    amioConnector = new AmioConnector(settings)
+    amioApi = new AmioApi(settings)
   })
 
-  it('Send Message', async () => {
-    const channel = {
-      id: config.CONNECTOR_CHANNEL_ID
-    }
-    const contact = {
-      id: config.CONNECTOR_CONTACT_ID
-    }
-    const content = {
-      type: 'text',
-      payload: 'Test message from Amio SDK'
-    }
-    const metadata = {
-      note: 'Sent by test in Amio SDK'
-    }
-    const sentMessage = await amioConnector.sendMessage(channel, contact, content, metadata, config.CONNECTOR_ACCESS_TOKEN)
+  describe('messages', () => {
+    it('send a message', async () => {
+      const channel = {
+        id: config.CONNECTOR_CHANNEL_ID
+      }
+      const contact = {
+        id: config.CONNECTOR_CONTACT_ID
+      }
+      const content = {
+        type: 'text',
+        payload: 'Test message from Amio SDK'
+      }
+      const metadata = {
+        note: 'Sent by test in Amio SDK'
+      }
+      const sentMessage = await amioApi.messages.send({
+        channel, contact, content, metadata
+      }, config.CONNECTOR_ACCESS_TOKEN)
 
-    expect(sentMessage.id).to.exist
+      expect(sentMessage.id).to.exist
 
-    expect(omit('id', sentMessage)).to.eql({
-      channel: assoc('type', 'facebook_messenger', channel),
-      contact,
-      content,
-      metadata
+      expect(omit('id', sentMessage)).to.eql({
+        channel: assoc('type', 'facebook_messenger', channel),
+        contact,
+        content,
+        metadata
+      })
     })
-  }).timeout(10000) // send message takes from 5s to 10s
+  })
+
 
 })
