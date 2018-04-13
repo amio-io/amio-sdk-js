5081fa467222b956c3d56990cc956664d309a746
zatziky <zatziky@gmail.com>
2018-04-13 12:57:23 +0200

    test-feat: don't run tests without proper config

--- test/config.js
+++ test/config.js
@@ -1,8 +1,19 @@
+const forEachObjIndexed = require('ramda/src/forEachObjIndexed')
+
+// we set config for amio+sdk_tests@amio.io in prod
 const config = {
-  CONNECTOR_API_BASE_URL: process.env.CONNECTOR_API_BASE_URL || 'https://api.amio.io',
+  CONNECTOR_API_BASE_URL: 'https://api.amio.io',
   CONNECTOR_ACCESS_TOKEN: process.env.CONNECTOR_ACCESS_TOKEN,
   CONNECTOR_CHANNEL_ID: process.env.CONNECTOR_CHANNEL_ID,
   CONNECTOR_CONTACT_ID: process.env.CONNECTOR_CONTACT_ID
 }
+
+forEachObjIndexed((value, key) => {
+  if(!value) {
+    const message = `All config values must be set so that tests can run. ${key} is missing.`;
+    console.error(message)
+    throw new Error(message)
+  }
+}, config)
 
 module.exports = config
