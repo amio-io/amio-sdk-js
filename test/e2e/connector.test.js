24eccb0cb0a683b6ab9ef59dd736f8a482fb3101
zatziky <zatziky@gmail.com>
2018-04-19 14:24:48 +0200

    test+ref: notification

--- test/e2e/connector.test.js
+++ test/e2e/connector.test.js
@@ -4,20 +4,23 @@ const expect = require('chai').expect
 const omit = require('ramda/src/omit')
 const assoc = require('ramda/src/assoc')
 
-let amioApi = null
+const settings = {
+  accessToken: config.CONNECTOR_ACCESS_TOKEN
+}
+const amioApi = new AmioApi(settings)
+
+const channel = {
+  id: config.CONNECTOR_CHANNEL_ID
+}
+const contact = {
+  id: config.CONNECTOR_CONTACT_ID
+}
 
 describe('Amio API Connector', function () {
 
-  before(() => {
-    const settings = {
-      baseUrl: config.CONNECTOR_API_BASE_URL
-    }
-    amioApi = new AmioApi(settings)
-  })
-
   describe('API error', () => {
     it('catches a 422 error', async () => {
-      await amioApi.messages.send({})
+      await amioApi.messages.send({}, 'wrong access token')
         .then(() => {
           throw new Error('exception was expected')
         })
@@ -37,14 +40,23 @@ describe('Amio API Connector', function () {
     })
   })
 
+  describe('notifications', () => {
+    it('send a notification', async () => {
+
+      const type = 'typing_on'
+      const notification = await amioApi.notifications.send({
+        channel, contact, type
+      })
+
+      expect(notification).to.eql({
+        channel: assoc('type', 'facebook_messenger', channel),
+        contact, type
+      })
+    })
+  })
+
   describe('messages', () => {
     it('send a message', async () => {
-      const channel = {
-        id: config.CONNECTOR_CHANNEL_ID
-      }
-      const contact = {
-        id: config.CONNECTOR_CONTACT_ID
-      }
       const content = {
         type: 'text',
         payload: 'Test message from Amio SDK'
@@ -54,15 +66,13 @@ describe('Amio API Connector', function () {
       }
       const sentMessage = await amioApi.messages.send({
         channel, contact, content, metadata
-      }, config.CONNECTOR_ACCESS_TOKEN)
+      })
 
       expect(sentMessage.id).to.exist
 
       expect(omit('id', sentMessage)).to.eql({
         channel: assoc('type', 'facebook_messenger', channel),
-        contact,
-        content,
-        metadata
+        contact, content, metadata
       })
     })
   })
