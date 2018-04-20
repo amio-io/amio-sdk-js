050391def3d70613eced8b9e3181bc91dc5fea59
Nobo Nadie <zatziky@gmail.com>
2018-04-20 14:44:44 +0200

    feat+test: contacts endpoint + tests

--- test/e2e/connector.test.js
+++ test/e2e/connector.test.js
@@ -40,6 +40,32 @@ describe('Amio API Connector', function () {
     })
   })
 
+  describe('contacts', () => {
+    it('finds a contact', async () => {
+      // const
+      //   channel_id = channel.id,
+      //   contact_id = contact.id
+
+      const contactFound = await amioApi.contacts.get(channel.id, contact.id)
+
+      expect(contactFound).to.include.all.keys('id', 'name')
+      expect(contactFound).to.have.any.keys('gender', 'locale', 'country', 'time_zone', 'photo_url')
+      expect(contactFound.id).to.eql(contact.id)
+    })
+
+    it('lists all contacts', async () => {
+      const contactList = await amioApi.contacts.list(channel.id)
+
+      expect(contactList).to.be.an('array')
+    })
+
+    it.skip('deletes a contact', async () => {
+      const contactDeleted = await amioApi.contacts.delete(channel.id, contact.id)
+
+      expect(contactDeleted).to.be.null
+    })
+  })
+
   describe('notifications', () => {
     it('send a notification', async () => {
 
@@ -49,10 +75,10 @@ describe('Amio API Connector', function () {
       })
 
       expect(notification).to.eql({
-          channel: {
-            id: channel.id,
-            type: notification.channel.type
-          },
+        channel: {
+          id: channel.id,
+          type: notification.channel.type
+        },
         contact, type
       })
     })
