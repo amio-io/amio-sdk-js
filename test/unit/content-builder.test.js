490b48d5ed6173d715d08947d5bebb77f926ca71
zatziky <zatziky@gmail.com>
2018-11-08 16:40:17 +0100

    feat: structure content builder

--- test/unit/content-builder.test.js
+++ test/unit/content-builder.test.js
@@ -1,9 +1,63 @@
+const ContentBuilder = require('../../lib/util/content-builder')
+const expect = require('chai').expect
+
+
 describe('ContentBuilder', () => {
 
-  describe('CommonBuilder', () => {})
+  describe('GenericBuilder', () => {
+
+
+
+
+
+  })
 
   describe('TextBuilder', () => {})
 
-  describe('StructureBuilder', () => {})
+  describe('StructureBuilder', () => {
+
+    it('1 card with all fields', () => {
+        const content = ContentBuilder.typeStructure()
+          .setText('text')
+          .setTitle('title')
+          .addButtonPostback('postback', 'payload')
+          .addButtonUrl('url', 'https://amio.io')
+          .addButtonCall('phone', '+0123456789')
+          .addQuickReply('xxx')
+          .build();
+
+        expect(content).to.eql({
+          type: 'structure',
+          payload: {
+            text: 'text',
+            title: 'title',
+            buttons: [
+              {type: 'postback', title: 'postback', payload: 'payload'},
+              {type: 'url', title: 'url', payload: 'https://amio.io'},
+              {type: 'phone', title: 'phone', payload: '+0123456789'},
+            ]
+          },
+          quick_replies: [{type: 'xxx'}]
+        })
+    })
+
+    it('2 cards', () => {
+        const content = ContentBuilder.typeStructure()
+          .setText('text')
+          .addNextCard()
+          .setText('text2')
+          .build();
+
+        expect(content).to.eql({
+          type: 'structure',
+          payload: [
+            {text: 'text'},
+            {text: 'text2'}
+          ]
+        })
+    })
+
+
+  })
 
 })
