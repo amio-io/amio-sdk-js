0bd4314cd1224fc8e293eedcf1a4802cca4a1bb1
zatziky <zatziky@gmail.com>
2018-11-08 16:52:15 +0100

    test: text content builder

--- test/unit/content-builder.test.js
+++ test/unit/content-builder.test.js
@@ -1,60 +1,68 @@
 const ContentBuilder = require('../../lib/util/content-builder')
 const expect = require('chai').expect
 
+const quickReply = {type: 'location'}
+const quick_replies = [quickReply]
 
 describe('ContentBuilder', () => {
 
   describe('GenericBuilder', () => {
 
 
-
-
-
   })
 
-  describe('TextBuilder', () => {})
+  describe('TextBuilder', () => {
+    it('Text and QuickReply', () => {
+      const payload = 'text';
+      const content = ContentBuilder.typeText(payload)
+        .addQuickReply(quickReply)
+        .build()
+
+      expect(content).to.eql({type: 'text', payload, quick_replies})
+    })
+  })
 
   describe('StructureBuilder', () => {
 
     it('1 card with all fields', () => {
-        const content = ContentBuilder.typeStructure()
-          .setText('text')
-          .setTitle('title')
-          .addButtonPostback('postback', 'payload')
-          .addButtonUrl('url', 'https://amio.io')
-          .addButtonCall('phone', '+0123456789')
-          .addQuickReply('xxx')
-          .build();
+      const content = ContentBuilder.typeStructure()
+        .setText('text')
+        .setTitle('title')
+        .addButtonPostback('postback', 'payload')
+        .addButtonUrl('url', 'https://amio.io')
+        .addButtonCall('phone', '+0123456789')
+        .addQuickReply(quickReply)
+        .build();
 
-        expect(content).to.eql({
-          type: 'structure',
-          payload: {
-            text: 'text',
-            title: 'title',
-            buttons: [
-              {type: 'postback', title: 'postback', payload: 'payload'},
-              {type: 'url', title: 'url', payload: 'https://amio.io'},
-              {type: 'phone', title: 'phone', payload: '+0123456789'},
-            ]
-          },
-          quick_replies: [{type: 'xxx'}]
-        })
+      expect(content).to.eql({
+        type: 'structure',
+        payload: {
+          text: 'text',
+          title: 'title',
+          buttons: [
+            {type: 'postback', title: 'postback', payload: 'payload'},
+            {type: 'url', title: 'url', payload: 'https://amio.io'},
+            {type: 'phone', title: 'phone', payload: '+0123456789'},
+          ]
+        },
+        quick_replies
+      })
     })
 
     it('2 cards', () => {
-        const content = ContentBuilder.typeStructure()
-          .setText('text')
-          .addNextCard()
-          .setText('text2')
-          .build();
+      const content = ContentBuilder.typeStructure()
+        .setText('text')
+        .addNextCard()
+        .setText('text2')
+        .build();
 
-        expect(content).to.eql({
-          type: 'structure',
-          payload: [
-            {text: 'text'},
-            {text: 'text2'}
-          ]
-        })
+      expect(content).to.eql({
+        type: 'structure',
+        payload: [
+          {text: 'text'},
+          {text: 'text2'}
+        ]
+      })
     })
 
 
