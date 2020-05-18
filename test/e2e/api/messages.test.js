9d713c3162308f86e78b3518669ca5afe3990e40
Jan <jan.donatek@amio.io>
2020-05-18 10:18:58 +0200

    test-docs: how to run tests for FB on CCI

--- test/e2e/api/messages.test.js
+++ test/e2e/api/messages.test.js
@@ -14,6 +14,7 @@ const contact = {
   id: config.AMIO_CONTACT_ID
 }
 
+// To make these tests running, send a message to FB channel so that it does not trigger 'Message sent outside the 24 hour window'
 describe('Amio API', function () {
 
   describe('messages', () => {
